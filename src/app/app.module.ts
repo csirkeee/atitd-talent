8fc431a4ef3e78e20079a67b308b3319c69356ca
András Eisenberger <csirkeee@gmail.com>
2019-10-26 16:19:01 +0100

    Set up basic page structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { FlexLayoutModule } from "@angular/flex-layout";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { SpecializationsComponent } from "./specializations/specializations.component";
+import { StatsComponent } from "./stats/stats.component";
+import { TestsComponent } from "./tests/tests.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    TestsComponent,
+    SpecializationsComponent,
+    StatsComponent,
   ],
   imports: [
     BrowserModule,
