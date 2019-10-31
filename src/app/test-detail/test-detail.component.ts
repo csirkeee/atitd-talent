d481355e725207d34ace9eec35844a4456c93ad2
András Eisenberger <csirkeee@gmail.com>
2019-10-31 11:00:45 +0000

    Checkboxes work and count TP

--- src/app/test-detail/test-detail.component.ts
+++ src/app/test-detail/test-detail.component.ts
@@ -1,6 +1,6 @@
 import { animate, state, style, transition, trigger } from "@angular/animations";
 import { Component, Input, OnInit } from "@angular/core";
-import { TestData } from "../tests/test-data";
+import { TaskData, TestData } from "../tests/test-data";
 
 @Component({
   selector: "app-test-detail",
@@ -20,12 +20,40 @@ export class TestDetailComponent implements OnInit {
 
   private expanded = false;
 
+  private checked = new Set();
+
+  private gotTp = 0;
+  private availableTp = 0;
+
   constructor() { }
 
   public ngOnInit() {
+    this.recalculate();
   }
 
   private toggleExpanded() {
     this.expanded = !this.expanded;
+  }
+
+  private onCheck(task: TaskData) {
+    if(this.checked.has(task.id)) {
+      this.checked.delete(task.id);
+    } else {
+      this.checked.add(task.id);
+    }
+    this.recalculate();
+  }
+
+  private recalculate() {
+    let gotTpCalc = 0;
+    let availableTpCalc = 0;
+    for(const task of this.testData.tasks) {
+      availableTpCalc += task.value;
+      if(this.checked.has(task.id)) {
+        gotTpCalc += task.value;
+      }
+    }
+    this.gotTp = gotTpCalc;
+    this.availableTp = availableTpCalc;
   }
 }
