62b49fa49a9b59e60e10077001e57a93fd5ca828
András Eisenberger <csirkeee@gmail.com>
2019-11-10 14:13:53 +0000

    Added all current tests to data

--- src/assets/atitd-data.ts
+++ src/assets/atitd-data.ts
@@ -3,34 +3,29 @@ import { GameData } from "src/app/game-data";
 export const ATITD_DATA: GameData = {
   tests: [
     {
-      id: "alignment",
-      name: "Astrological Alignment",
-      imageLocation: "assets\\images\\tests\\Worship_Test_Icons_Astro_Alignment.png",
+      id: "starting",
+      name: "Starting tasks",
+      imageLocation: "assets\\images\\tests\\Leadership_Test_Icons_Plantation.png",
       tasks: [
         {
-          id: "found5",
-          text: "Found 5 aligned people",
+          id: "citizenship",
+          text: "Become a citizen of Egypt",
           value: 1,
         },
         {
-          id: "found15",
-          text: "Found 15 aligned people",
+          id: "homesweethome",
+          text: "Complete the Home Sweet Home task",
           value: 1,
         },
         {
-          id: "found30",
-          text: "Found 30 aligned people",
+          id: "artandmusic",
+          text: "Complete the Art and Music task",
           value: 1,
         },
         {
-          id: "found45",
-          text: "Found 45 aligned people",
+          id: "thought",
+          text: "Complete the Puzzle in the Night Sky task",
           value: 1,
-        },
-        {
-          id: "ritual",
-          text: "Completed ritual",
-          value: 3,
         },
       ],
     },
@@ -72,6 +67,494 @@ export const ATITD_DATA: GameData = {
         {
           id: "heavenly",
           text: "Built and held a Heavenly Obelisk",
+          value: 1,
+        },
+      ],
+    },
+    {
+      id: "towers",
+      name: "Towers",
+      imageLocation: "assets\\images\\tests\\Architecture_Test_Icons_Towers.png",
+      tasks: [
+        {
+          id: "solidland",
+          text: "Got 100% coverage with Solid Land towers",
+          value: 1,
+        },
+        {
+          id: "richsoil",
+          text: "Got 100% coverage with Rich Soil towers",
+          value: 1,
+        },
+        {
+          id: "handofman",
+          text: "Got 100% coverage with Hand of Man towers",
+          value: 1,
+        },
+        {
+          id: "livingland",
+          text: "Got 100% coverage with Living Land towers",
+          value: 1,
+        },
+        {
+          id: "newlife",
+          text: "Got 100% coverage with New Life towers",
+          value: 1,
+        },
+        {
+          id: "racingmind",
+          text: "Got 100% coverage with Racing Mind towers",
+          value: 1,
+        },
+        {
+          id: "night",
+          text: "Got 100% coverage with Night towers",
+          value: 1,
+        },
+      ],
+    },
+    {
+      id: "cicada",
+      name: "Singing Cicada",
+      imageLocation: "assets\\images\\tests\\Human_Body_Test_Icons_Cicada.png",
+      tasks: [
+        {
+          id: "pass1st",
+          text: "Get your 1st pass by collecting enough points",
+          value: 1,
+        },
+        {
+          id: "pass2nd",
+          text: "Get your 2nd pass by collecting enough points",
+          value: 1,
+        },
+        {
+          id: "pass3rd",
+          text: "Get your 3rd pass by collecting enough points",
+          value: 1,
+        },
+        {
+          id: "pass4th",
+          text: "Get your 4th pass by collecting enough points",
+          value: 1,
+        },
+        {
+          id: "pass5th",
+          text: "Get your 5th pass by collecting enough points",
+          value: 1,
+        },
+        {
+          id: "pass6th",
+          text: "Get your 6th pass by collecting enough points",
+          value: 1,
+        },
+        {
+          id: "pass7th",
+          text: "Get your 7th pass by collecting enough points",
+          value: 1,
+        },
+      ],
+    },
+    {
+      id: "bedouin",
+      name: "Bedouin",
+      imageLocation: "assets\\images\\tests\\Human_Body_Test_Icons_Bedouin.png",
+      tasks: [
+        {
+          id: "pass1st",
+          text: "Get your 1st pass by holding enough altars",
+          value: 1,
+        },
+        {
+          id: "pass2nd",
+          text: "Get your 2nd pass by holding enough altars",
+          value: 1,
+        },
+        {
+          id: "pass3rd",
+          text: "Get your 3rd pass by holding enough altars",
+          value: 1,
+        },
+        {
+          id: "pass4th",
+          text: "Get your 4th pass by holding enough altars",
+          value: 1,
+        },
+        {
+          id: "pass5th",
+          text: "Get your 5th pass by holding enough altars",
+          value: 1,
+        },
+        {
+          id: "pass6th",
+          text: "Get your 6th pass by holding enough altars",
+          value: 1,
+        },
+        {
+          id: "pass7th",
+          text: "Get your 7th pass by holding enough altars",
+          value: 1,
+        },
+      ],
+    },
+    {
+      id: "oyster",
+      name: "Oyster Catcher",
+      imageLocation: "assets\\images\\tests\\Human_Body_Test_Icons_Oyster_Catcher.png",
+      tasks: [
+        {
+          id: "pass1st",
+          text: "Get your 1st point by putting a pearl in your necklace",
+          value: 1,
+        },
+        {
+          id: "pass2nd",
+          text: "Get your 2nd point by making progress on your necklace",
+          value: 1,
+        },
+        {
+          id: "pass3rd",
+          text: "Get your 3rd point by making progress on your necklace",
+          value: 1,
+        },
+        {
+          id: "pass4th",
+          text: "Get your 4th point by making progress on your necklace",
+          value: 1,
+        },
+        {
+          id: "pass5th",
+          text: "Get your 5th point by making progress on your necklace",
+          value: 1,
+        },
+        {
+          id: "pass6th",
+          text: "Get your 6th point by making progress on your necklace",
+          value: 1,
+        },
+        {
+          id: "complete",
+          text: "Get your 7th point by completing your necklace",
+          value: 1,
+        },
+      ],
+    },
+    {
+      id: "acrobat",
+      name: "Acrobat",
+      imageLocation: "assets\\images\\tests\\Human_Body_Test_Icons_Acrobat.png",
+      tasks: [
+        {
+          id: "teach7",
+          text: "Teach 7 facets to other players",
+          value: 1,
+        },
+        {
+          id: "learn8",
+          text: "Learn 8 moves",
+          value: 1,
+        },
+        {
+          id: "learn12",
+          text: "Learn 12 moves",
+          value: 1,
+        },
+        {
+          id: "learn16",
+          text: "Learn 16 moves",
+          value: 1,
+        },
+        {
+          id: "learn20",
+          text: "Learn 20 moves",
+          value: 1,
+        },
+        {
+          id: "learn24",
+          text: "Learn 24 moves",
+          value: 1,
+        },
+        {
+          id: "learn28",
+          text: "Learn 28 moves",
+          value: 1,
+        },
+      ],
+    },
+    {
+      id: "tattoo",
+      name: "Ritual Tattoo",
+      imageLocation: "assets\\images\\tests\\Human_Body_Test_Icons_Ritual_Tattoo.png",
+      tasks: [
+        {
+          id: "complete1stlist",
+          text: "Hand in 4 complete mortars from your 1st list",
+          value: 1,
+        },
+        {
+          id: "complete2ndlist",
+          text: "Hand in 4 complete mortars from your 2nd list",
+          value: 1,
+        },
+        {
+          id: "complete3rdlist",
+          text: "Hand in 4 complete mortars from your 3rd list",
+          value: 1,
+        },
+        {
+          id: "complete4thlist",
+          text: "Hand in 4 complete mortars from your 4th list",
+          value: 1,
+        },
+        {
+          id: "complete5thlist",
+          text: "Hand in 4 complete mortars from your 5th list",
+          value: 1,
+        },
+        {
+          id: "complete6thlist",
+          text: "Hand in 4 complete mortars from your 6th list",
+          value: 1,
+        },
+        {
+          id: "complete7thlist",
+          text: "Hand in 4 complete mortars from your 7th list",
+          value: 1,
+        },
+      ],
+    },
+    {
+      id: "safari",
+      name: "Safari",
+      imageLocation: "assets\\images\\tests\\Human_Body_Test_Safari.png",
+      tasks: [
+        {
+          id: "cought4",
+          text: "Catch a total of 4 safari animals",
+          value: 1,
+        },
+        {
+          id: "cought8",
+          text: "Catch a total of 8 safari animals",
+          value: 1,
+        },
+        {
+          id: "cought12",
+          text: "Catch a total of 12 safari animals",
+          value: 1,
+        },
+        {
+          id: "cought16",
+          text: "Catch a total of 16 safari animals",
+          value: 1,
+        },
+        {
+          id: "cought20",
+          text: "Catch a total of 20 safari animals",
+          value: 1,
+        },
+        {
+          id: "cought24",
+          text: "Catch a total of 24 safari animals",
+          value: 1,
+        },
+        {
+          id: "cought28",
+          text: "Catch a total of 28 safari animals",
+          value: 1,
+        },
+      ],
+    },
+    {
+      id: "darkestnight",
+      name: "Darkest Night",
+      imageLocation: "assets\\images\\tests\\Human_Body_Test_Icons_Darkest_Night.png",
+      tasks: [
+        {
+          id: "complete1st",
+          text: "Ingest all mushrooms on your 1st list",
+          value: 1,
+        },
+        {
+          id: "complete2nd",
+          text: "Ingest all mushrooms on your 2nd list",
+          value: 1,
+        },
+        {
+          id: "complete3rd",
+          text: "Ingest all mushrooms on your 3rd list",
+          value: 1,
+        },
+        {
+          id: "complete4th",
+          text: "Ingest all mushrooms on your 4th list",
+          value: 1,
+        },
+        {
+          id: "complete5th",
+          text: "Ingest all mushrooms on your 5th list",
+          value: 1,
+        },
+        {
+          id: "complete6th",
+          text: "Ingest all mushrooms on your 6th list",
+          value: 1,
+        },
+        {
+          id: "complete7th",
+          text: "Ingest all mushrooms on your 7th list",
+          value: 1,
+        },
+      ],
+    },
+    {
+      id: "khefreschildren",
+      name: "Khefre's Children",
+      imageLocation: "assets\\images\\tests\\Art_and_Music_Test_Icons_Khefres_Child.png",
+      tasks: [
+        {
+          id: "breedowned",
+          text: "Breed a beetle to 90% - 99% Ownership",
+          value: 1,
+        },
+        {
+          id: "winlevel1",
+          text: "Win a Level 1 Garden",
+          value: 1,
+        },
+        {
+          id: "winlevel2",
+          text: "Win a Level 2 Garden",
+          value: 2,
+        },
+        {
+          id: "winlevel3",
+          text: "Win a Level 3 Garden",
+          value: 3,
+        },
+      ],
+    },
+    {
+      id: "alignment",
+      name: "Astrological Alignment",
+      imageLocation: "assets\\images\\tests\\Worship_Test_Icons_Astro_Alignment.png",
+      tasks: [
+        {
+          id: "found5",
+          text: "Found 5 aligned people",
+          value: 1,
+        },
+        {
+          id: "found15",
+          text: "Found 15 aligned people",
+          value: 1,
+        },
+        {
+          id: "found30",
+          text: "Found 30 aligned people",
+          value: 1,
+        },
+        {
+          id: "found45",
+          text: "Found 45 aligned people",
+          value: 1,
+        },
+        {
+          id: "ritual",
+          text: "Completed ritual",
+          value: 3,
+        },
+      ],
+    },
+    {
+      id: "octec",
+      name: "Octec's Ghost",
+      imageLocation: "assets\\images\\tests\\Architecture_Test_Icons_Octec_Ghost.png",
+      tasks: [
+        {
+          id: "built",
+          text: "Build the Statue of Octec's Ghost",
+          value: 1,
+        },
+        {
+          id: "placed2",
+          text: "Install 2 Crystal Hieroglyphs",
+          value: 1,
+        },
+        {
+          id: "placed5",
+          text: "Install 5 Crystal Hieroglyphs",
+          value: 1,
+        },
+        {
+          id: "placed10",
+          text: "Install 10 Crystal Hieroglyphs",
+          value: 1,
+        },
+        {
+          id: "placed15",
+          text: "Install 15 Crystal Hieroglyphs",
+          value: 1,
+        },
+        {
+          id: "placed18",
+          text: "Install 18 Crystal Hieroglyphs",
+          value: 1,
+        },
+        {
+          id: "activate",
+          text: "Activate the statue",
+          value: 1,
+        },
+      ],
+    },
+    {
+      id: "balance",
+      name: "Balance of Goods",
+      imageLocation: "assets\\images\\tests\\Harmony_Test_Icons_Balance_Of_Goods.png",
+      tasks: [
+        {
+          id: "built",
+          text: "Build the Offering Vault",
+          value: 1,
+        },
+        {
+          id: "won",
+          text: "Won the weekly offering count",
+          value: 6,
+        },
+      ],
+    },
+    {
+      id: "marriage",
+      name: "Marriage",
+      imageLocation: "assets\\images\\tests\\Harmony_Test_Icons_Marriage.png",
+      tasks: [
+        {
+          id: "witness",
+          text: "Witness a wedding",
+          value: 1,
+        },
+        {
+          id: "won",
+          text: "Won the weekly score count",
+          value: 6,
+        },
+      ],
+    },
+    {
+      id: "vigil",
+      name: "Vigil",
+      imageLocation: "assets\\images\\tests\\Worship_Test_Icons_Vigil.png ",
+      tasks: [
+        {
+          id: "build",
+          text: "Build a sacrificial bonfire or make a sacrifice",
+          value: 1,
+        },
+        {
+          id: "sacrifice",
+          text: "Make a sacrifice to a sacrificial bonfire",
           value: 1,
         },
       ],
